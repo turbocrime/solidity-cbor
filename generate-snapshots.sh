57d755bffba6e8735f158b33c81cdacb6117bed2
turbocrime <turbocrime@users.noreply.github.com>
2025-01-17 20:22:31 -0800

    reorganize snapshots

--- generate-snapshots.sh
+++ generate-snapshots.sh
@@ -6,9 +6,12 @@ unset FORGE_GAS_REPORT
 unset FOUNDRY_PROFILE
 unset FOUNDRY_VIA_IR
 
-for test_profile in test-no-ir test-via-ir; do
-  export FOUNDRY_PROFILE=$test_profile
-  forge coverage --force --report-file $test_profile.lcov.info --report lcov --report summary --no-match-path "test/comparison/*" --no-match-coverage "test/comparison/*"
-  forge snapshot --force --snap $test_profile.gas-snapshot --match-path "test/comparison/*"
+mkdir -p coverage
+forge coverage --force --report-file coverage/lcov.info --report lcov --report summary --no-match-path "test/comparison/*" --no-match-coverage "test/comparison/*"
+
+for test_profile in no-ir via-ir; do
+  export FOUNDRY_PROFILE=test-$test_profile
+  mkdir -p snapshots/$test_profile
+  forge snapshot --force --snap snapshots/$test_profile/.gas-snapshot --match-path "test/comparison/*"
   forge test --force -vvv --no-match-path "test/comparison/*"
 done
